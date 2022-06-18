4c3c9d66f9cfa1b629078f7addd4dba4b12617c3
Vasyl Kulyk <detroit.vv@gmail.com>
2022-06-19 00:19:41 +0300

    If Statements

--- test.js
+++ test.js
@@ -1,8 +1,32 @@
-// the first alert
-alert("Hello, World!"); 
-// the second alert
-Alert("Hello, World!");
+var youLikeMeat = true;
+var myNum = 10;
 
-// make the image invisible
-// move the image from top to bottom
-// make the image visible
+if (youLikeMeat) {
+    document.write("Here is the meaty menu...");
+}
+
+if (7<5) {
+    document.write("this expression is true");
+}
+
+if (myNum > 10) {
+    document.write("myNum is greater than 10");
+}
+
+if (myNum == 10) {
+    document.write("myNum is equal 10");
+}
+
+var myNum = 8;
+
+if (myNum == 10) {
+    document.write("myNum is equal 10");
+} else {
+    document.write("myNum is not equal 10");
+}
+
+if (youLikeMeat) {
+    document.write("you like meat");
+} else {
+    document.write("you hate meat");
+}

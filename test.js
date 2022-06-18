a29c8f69a8124b0687ef90aef1fd279485d7f7f3
Vasyl Kulyk <detroit.vv@gmail.com>
2022-06-19 00:25:06 +0300

    Else If Statements

--- test.js
+++ test.js
@@ -1,32 +1,12 @@
-var youLikeMeat = true;
+var myAge = 29;
 var myNum = 10;
 
-if (youLikeMeat) {
-    document.write("Here is the meaty menu...");
-}
-
-if (7<5) {
-    document.write("this expression is true");
-}
-
-if (myNum > 10) {
-    document.write("myNum is greater than 10");
-}
-
-if (myNum == 10) {
-    document.write("myNum is equal 10");
-}
-
-var myNum = 8;
-
-if (myNum == 10) {
-    document.write("myNum is equal 10");
+if (myAge > 30) {
+    document.write("you are over 30!");
+} else if (myAge >20) {
+    document.write("you are over 20!");
+} else if (myAge>10){
+    document.write("you are over 10!");
 } else {
-    document.write("myNum is not equal 10");
-}
-
-if (youLikeMeat) {
-    document.write("you like meat");
-} else {
-    document.write("you hate meat");
+    document.write("you are not over 10!");
 }

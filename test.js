a80cfbca521c2eebd2bd3e7e5c5ff52ed458b8f4
Vasyl Kulyk <detroit.vv@gmail.com>
2022-06-19 00:39:22 +0300

    Logical Operators

--- test.js
+++ test.js
@@ -1,12 +1,21 @@
-var myAge = 29;
-var myNum = 10;
+var myAge = 25;
 
-if (myAge > 30) {
-    document.write("you are over 30!");
-} else if (myAge >20) {
-    document.write("you are over 20!");
-} else if (myAge>10){
-    document.write("you are over 10!");
-} else {
-    document.write("you are not over 10!");
+if(myAge >=18 && myAge<=30){
+    document.write("You can come, you cool dude");
+}else{
+    document.write("you aint coming!");
+}
+
+if(myAge <18 || myAge>30 || myAge === 25){
+    document.write("you aint coming!");
+}else{
+    document.write("You can come, you cool dude");
+}
+
+myAge = 25;
+
+if(myAge <18 || myAge>30 || myAge === 25){
+    document.write("you aint coming!");
+}else{
+    document.write("You can come, you cool dude");
 }

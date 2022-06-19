dd84b6ebf2fe0d8562f7bb1e51f2b9b36d91254b
Vasyl Kulyk <detroit.vv@gmail.com>
2022-06-19 09:20:23 +0300

    Practical Example using Loops

--- test.js
+++ test.js
@@ -1,13 +1,6 @@
+// array
+var links = document.getElementsByTagName("a");
 
-for (i = 0; i < 10; i++) {
-
-    if (i === 5 || i === 3) {
-        continue;
-    }
-    console.log(i);
-    if (i === 7) {
-        break;
-    }
+for (i = 0; i < links.length; i++) {
+    links[i].className = "link-" + i;
 }
-
-console.log("I have broken out of the loop")

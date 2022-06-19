a539025b6b2e074eec617b4a24d858c4c6a7883a
Vasyl Kulyk <detroit.vv@gmail.com>
2022-06-19 23:35:19 +0300

    Constructor Functions

--- test.js
+++ test.js
@@ -1,14 +1,18 @@
-var myCar2 = {
-    masSpeed: 70,
-    driver: "Net Ninja",
-    drive: function (speed, time) {
+var Car = function (maxSpeed, driver){
+    this.maxSpeed = maxSpeed;
+    this.driver = driver;
+    this.drive = function (speed, time) {
         console.log(speed * time);
-    },
-    logDriver: function(){
+    }
+    this.logDriver = function(){
         console.log("driver name is " + this.driver)
     }
-};
+}
 
-myCar2.logDriver();
-console.log(myCar2.masSpeed);
-myCar2.drive(50,3);
+var myCar = new Car(50, "Ninja");
+var myCar2 = new Car(40, "Humpty Dumpty");
+var myCar3 = new Car(10, "Shaun");
+var myCar4 = new Car(90, "James Bond");
+
+myCar.drive(30,5);
+myCar3.logDriver();

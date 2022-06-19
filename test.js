77c34524d7758ed748bc150de0ec019f13f7cdbd
Vasyl Kulyk <detroit.vv@gmail.com>
2022-06-19 17:44:08 +0300

    Creating a new JavaScript Object

--- test.js
+++ test.js
@@ -1,42 +1,36 @@
-var myString = "I am a string";
-var myString = 'I am a "fun" string';
-var myString = 'I\'m a "fun" string';
+var myArray = new Array();
+myArray[0] = 8;
+myArray[1] = "hello";
 
-console.log(myString.length);
-console.log(myString.toUpperCase);
-console.log(myString.toLowerCase);
-console.log(myString.indexOf("string"));
-console.log(myString.indexOf("ninja"));
+var myCar = new Object();
+myCar.maxSpeed = 50;
+myCar.driver = "Shaun";
 
-if (myString.indexOf("ninja") === -1) {
-    console.log("the word ninja is not on the string");
-} else {
-    console.log("the word ninja starts at position " +
-        myString.indexOf("ninja"));
-}
+console.log(myCar.driver);
 
-9 === 8
-9 == 8
+myCar.drive = function () {
+    console.log("now driving");
+};
 
-var string1 = "abc";
-var string2 = "bcd";
-console.log(string1 === string2);
+myCar.drive();
 
-var string1 = "abc";
-var string2 = "BCD";
-console.log(string1.toLowerCase === string2.toLowerCase);
+var myCar2 = {
+    masSpeed: 70,
+    driver: "Net Ninja",
+    drive: function () {
+        console.log("now driving");
+    }
+};
 
-// true, why ???
-var string1 = "abc";
-var string2 = "bcd";
-console.log(string1.toLowerCase === string2.toLowerCase);
+console.log(myCar2.masSpeed);
+myCar2.drive();
 
-// Because of order in alphabet // true
-var string1 = "abc";
-var string2 = "bcd";
-console.log(string1 < string2);
+var myCar2 = {
+    masSpeed: 70,
+    driver: "Net Ninja",
+    drive: function (speed, time) {
+        console.log(speed * time);
+    }
+};
 
-// false
-var string1 = "abc";
-var string2 = "Bcd";
-console.log(string1 < string2);
+myCar2.drive(50,3);

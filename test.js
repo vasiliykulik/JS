a6faea5493ac6a4d18ef0ca93432e9ae5313f64c
Vasyl Kulyk <detroit.vv@gmail.com>
2022-06-19 08:57:40 +0300

    Break & Continue

--- test.js
+++ test.js
@@ -1,7 +1,13 @@
-var links = document.getElementsByTagName("a");
 
-for (i = 0; i < links.length; i++) {
-    console.log("this is link number " + i);
+for (i = 0; i < 10; i++) {
+
+    if (i === 5 || i === 3) {
+        continue;
+    }
+    console.log(i);
+    if (i === 7) {
+        break;
+    }
 }
 
-document.write("all links now looped");
+console.log("I have broken out of the loop")

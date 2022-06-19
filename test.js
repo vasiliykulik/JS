2946c642fcf3f7aeb8736fd20a237ac7dff68a9f
Vasyl Kulyk <detroit.vv@gmail.com>
2022-06-19 23:50:13 +0300

    The Date Object

--- test.js
+++ test.js
@@ -1,18 +1,33 @@
-var Car = function (maxSpeed, driver){
-    this.maxSpeed = maxSpeed;
-    this.driver = driver;
-    this.drive = function (speed, time) {
-        console.log(speed * time);
-    }
-    this.logDriver = function(){
-        console.log("driver name is " + this.driver)
-    }
+var myDate = new Date();
+console.log(myDate);
+
+var myPastDate = new Date(1545, 11, 2);
+var myFutureDate = new Date(2515, 0, 31);
+
+console.log(myPastDate, 10, 30, 15);
+console.log(myFutureDate, 10, 30, 15);
+
+var birthday = new Date(1985, 0, 15, 11, 15, 25);
+var birthday2 = new Date(1985, 0, 15, 11, 15, 25);
+
+console.log(birthday.getMonth());
+console.log(birthday.getFullYear());
+console.log(birthday.getDate());
+console.log(birthday.getDay());
+console.log(birthday.getHours());
+// get the number of milliseconds since 1st Jan 1970
+// used to compare two dates
+console.log(birthday.getTime());
+
+if(birthday == birthday2){
+    console.log("birthdays are equal");
+} else {
+    console.log("birthdays are not equal");
 }
+// false we compare links on objects
 
-var myCar = new Car(50, "Ninja");
-var myCar2 = new Car(40, "Humpty Dumpty");
-var myCar3 = new Car(10, "Shaun");
-var myCar4 = new Car(90, "James Bond");
-
-myCar.drive(30,5);
-myCar3.logDriver();
+if(birthday.getTime == birthday2.getTime){
+    console.log("birthdays are equal");
+} else {
+    console.log("birthdays are not equal");
+}

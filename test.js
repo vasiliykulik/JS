e4b659400d797496dcfed0378274f6058a50d869
Vasyl Kulyk <detroit.vv@gmail.com>
2022-06-19 12:49:08 +0300

    Numbers

--- test.js
+++ test.js
@@ -1,17 +1,15 @@
-var average;
+var a = 5;
+var b = 5;
+console.log (a+b);
+console.log(typeof(a+b));
 
-function getAverage(a, b) {
+var a = 5;
+var b = "5";
+console.log (a+b);
+console.log(typeof(a+b));
 
-    average = (a + b) / 2; // local variable
-    console.log(average);
-    return average;
-}
-
-var myResult = getAverage(7, 8); // global variable
-
-
-function logResult() {
-    console.log("the average is " + myResult + " inside the function");
-}
-
-logResult();
+console.log(Math.round(7.5));
+console.log(Math.floor(7.5));
+console.log(Math.ceil(7.5));
+console.log(Math.max(7,4,9,8));
+console.log(Math.PI);

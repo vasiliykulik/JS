f7cbedf0390197ca61894f2e54f4fb9230923ccc
Vasyl Kulyk <detroit.vv@gmail.com>
2022-06-19 09:31:22 +0300

    Functions

--- test.js
+++ test.js
@@ -1,6 +1,9 @@
-// array
-var links = document.getElementsByTagName("a");
+function getAverage(a, b, c, d, e, f) {
 
-for (i = 0; i < links.length; i++) {
-    links[i].className = "link-" + i;
+    var average = (a + b + c + d + e + f) / 2;
+    console.log(average);
+    return average;
 }
+
+var myResult = getAverage(7, 8, 9, 10, 11, 12);
+console.log("the average is " + myResult);

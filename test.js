c9cf1a33bd2edbc01405b67fe631d86b9648a50b
Vasyl Kulyk <detroit.vv@gmail.com>
2022-06-20 16:09:27 +0300

    Module Quiz

--- test.js
+++ test.js
@@ -4,22 +4,19 @@ var colours = ["red", "green", "blue", "pink"];
 var counter = 0;
 
 function changeColour() {
-    if (counter>= colours.length){
-        counter=0;
+    if (counter >= colours.length) {
+        counter = 0;
     }
     colourChanger.style.background = colours[counter];
     counter++;
 }
 
-var myTimer = setInterval(changeColour,3000);
+var myTimer = setInterval(changeColour, 3000);
 
-colourChanger.onclick = function(){
+colourChanger.onclick = function () {
     clearInterval(myTimer);
     colourChanger.innerHTML = "Timer stopped";
 }
-
-
-
 
 
 // var myMessage = document.getElementById("message");
@@ -30,3 +27,42 @@ colourChanger.onclick = function(){
 // }
 
 // setTimeout(showMessage, 3000); // once
+
+var num = 2;
+function func(num) {
+    var num;
+    console.log(num);
+    num = 5;
+}
+func(10);
+console.log(num);
+
+(function () {
+    console.log(1);
+    setTimeout(function () {console.log(2);}, 1000);
+    setTimeout(function () {console.log(3);}, 0);
+    console.log(4);
+})();
+
+
+console.log(+"1"+"1"+"2");
+console.log("A"-"B"+"2");
+console.log("A"-"B"+2);
+
+
+function func(x){
+    console.log(typeof x);
+    console.log(arguments.length);
+}
+
+func();
+func('1','2','3');
+
+const b = {}
+console.log(b == b);
+console.log(typeof null == 'object');
+
+const a = NaN
+console.log(a == a);
+
+console.log(!![] == true);

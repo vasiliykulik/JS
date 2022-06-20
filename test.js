c2f8064d64ab8aa2eb59296dc86f0c582db54604
Vasyl Kulyk <detroit.vv@gmail.com>
2022-06-20 15:40:37 +0300

    JavaScript Timers

--- test.js
+++ test.js
@@ -1,23 +1,32 @@
-function setUpEvents(){
 
-    var content = document.getElementById("content");
-    var button = document.getElementById("show-more");
+var colourChanger = document.getElementById("colour-changer");
+var colours = ["red", "green", "blue", "pink"];
+var counter = 0;
 
-    button.onclick = function () {
-        if (content.className == "open") {
-            // shrink the box
-            content.className = "";
-            button.innerHTML = "Show More";
-        } else {
-            // expand the box
-            content.className = "open";
-            button.innerHTML = "Show Less";
-        }
+function changeColour() {
+    if (counter>= colours.length){
+        counter=0;
     }
+    colourChanger.style.background = colours[counter];
+    counter++;
+}
 
+var myTimer = setInterval(changeColour,3000);
+
+colourChanger.onclick = function(){
+    clearInterval(myTimer);
+    colourChanger.innerHTML = "Timer stopped";
 }
 
 
-window.onload = function () {
-    setUpEvents();
-};
+
+
+
+// var myMessage = document.getElementById("message");
+
+// function showMessage(){
+//     myMessage.className = "show";
+
+// }
+
+// setTimeout(showMessage, 3000); // once
